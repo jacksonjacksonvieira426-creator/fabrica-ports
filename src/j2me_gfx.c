9b392e0c6bbd4a7dd272f3e4008b57105ca484bc
jacksonjacksonvieira426-creator <jacksonjacksonvieira426-creator@users.noreply.github.com>
2026-09-13 18:29:25 -0300

    Fase 4: motor grafico funcionando (VRAM uncached) - 4 quadrados

--- src/j2me_gfx.c
+++ src/j2me_gfx.c
@@ -1,21 +1,16 @@
 #include "j2me_gfx.h"
 #include <pspkernel.h>
 #include <pspdisplay.h>
-#include <pspgu.h>
-#include <pspgum.h>
 
-#define BUF_WIDTH  512
-#define SCR_WIDTH  J2ME_SCREEN_W
-#define SCR_HEIGHT J2ME_SCREEN_H
+#define SCR_W J2ME_SCREEN_W
+#define SCR_H J2ME_SCREEN_H
+#define STRIDE 512
 
-static unsigned int __attribute__((aligned(16))) list[262144];
+#define VRAM ((unsigned int*)0x44000000)
+
 static unsigned int cur_color_rgb = 0x000000;
 
-typedef struct {
-    unsigned short x, y, z;
-    unsigned int   color;
-} Vertex;
-
+// Converte 0xRRGGBB (J2ME) para ABGR (formato do PSP)
 static unsigned int rgb_to_psp(unsigned int rgb) {
     unsigned int r = (rgb >> 16) & 0xFF;
     unsigned int g = (rgb >>  8) & 0xFF;
@@ -24,55 +19,17 @@ static unsigned int rgb_to_psp(unsigned int rgb) {
 }
 
 void j2me_gfx_init(void) {
-    sceGuInit();
-    sceGuStart(GU_DIRECT, list);
-    sceGuDrawBuffer(GU_PSM_8888, (void*)0, BUF_WIDTH);
-    sceGuDispBuffer(SCR_WIDTH, SCR_HEIGHT, (void*)0x88000, BUF_WIDTH);
-    sceGuDepthBuffer((void*)0x110000, BUF_WIDTH);
-    sceGuOffset(2048 - (SCR_WIDTH/2), 2048 - (SCR_HEIGHT/2));
-    sceGuViewport(2048, 2048, SCR_WIDTH, SCR_HEIGHT);
-    sceGuDepthRange(0xC350, 0xFFFF);
-    sceGuScissor(0, 0, SCR_WIDTH, SCR_HEIGHT);
-    sceGuEnable(GU_SCISSOR_TEST);
-    sceGuDisable(GU_DEPTH_TEST);
-    sceGuDisable(GU_CULL_FACE);
-    sceGuDisable(GU_LIGHTING);
-    sceGuDisable(GU_BLEND);
-    sceGuDisable(GU_TEXTURE_2D);
-    sceGuDisable(GU_ALPHA_TEST);
-
-    // Matrixes explicitas (identidade)
-    sceGumMatrixMode(GU_PROJECTION);
-    sceGumLoadIdentity();
-    sceGumMatrixMode(GU_VIEW);
-    sceGumLoadIdentity();
-    sceGumMatrixMode(GU_MODEL);
-    sceGumLoadIdentity();
-
-    sceGuFinish();
-    sceGuSync(0, 0);
-    sceDisplayWaitVblankStart();
-    sceGuDisplay(GU_TRUE);
+    sceDisplaySetMode(0, SCR_W, SCR_H);
+    sceDisplaySetFrameBuf(VRAM, STRIDE, PSP_DISPLAY_PIXEL_FORMAT_8888,
+                          PSP_DISPLAY_SETBUF_IMMEDIATE);
 }
 
-void j2me_gfx_shutdown(void) {
-    sceGuDisplay(GU_FALSE);
-    sceGuTerm();
-}
+void j2me_gfx_shutdown(void) { }
 
-void j2me_gfx_begin_frame(void) {
-    sceGuStart(GU_DIRECT, list);
-}
-
-void j2me_gfx_clear(unsigned int rgb) {
-    sceGuClearColor(rgb_to_psp(rgb));
-    sceGuClear(GU_COLOR_BUFFER_BIT);
-}
+void j2me_gfx_begin_frame(void) { }
 
 void j2me_gfx_flip(void) {
-    sceGuFinish();
-    sceGuSync(0, 0);
-    sceGuSwapBuffers();
+    sceKernelDcacheWritebackAll();
     sceDisplayWaitVblankStart();
 }
 
@@ -80,25 +37,29 @@ void j2me_gfx_set_color(unsigned int rgb) {
     cur_color_rgb = rgb;
 }
 
+void j2me_gfx_clear(unsigned int rgb) {
+    unsigned int c = rgb_to_psp(rgb);
+    for (int y = 0; y < SCR_H; y++) {
+        unsigned int* row = VRAM + y * STRIDE;
+        for (int x = 0; x < SCR_W; x++) {
+            row[x] = c;
+        }
+    }
+}
+
 void j2me_gfx_fill_rect(int x, int y, int w, int h) {
+    if (w <= 0 || h <= 0) return;
+    if (x < 0) { w += x; x = 0; }
+    if (y < 0) { h += y; y = 0; }
+    if (x + w > SCR_W)  w = SCR_W - x;
+    if (y + h > SCR_H) h = SCR_H - y;
     if (w <= 0 || h <= 0) return;
 
     unsigned int c = rgb_to_psp(cur_color_rgb);
-
-    Vertex* v = (Vertex*)sceGuGetMemory(2 * sizeof(Vertex));
-    if (!v) return;
-
-    v[0].x = (unsigned short)x;
-    v[0].y = (unsigned short)y;
-    v[0].z = 0;
-    v[0].color = c;
-
-    v[1].x = (unsigned short)(x + w);
-    v[1].y = (unsigned short)(y + h);
-    v[1].z = 0;
-    v[1].color = c;
-
-    sceGuDrawArray(GU_SPRITES,
-                   GU_VERTEX_16BIT | GU_COLOR_8888 | GU_TRANSFORM_2D,
-                   2, 0, v);
+    for (int j = 0; j < h; j++) {
+        unsigned int* row = VRAM + (y + j) * STRIDE + x;
+        for (int i = 0; i < w; i++) {
+            row[i] = c;
+        }
+    }
 }
--- src/main.c
+++ src/main.c
@@ -1,51 +1,37 @@
 #include <pspkernel.h>
-#include <pspdisplay.h>
 #include <pspctrl.h>
+#include "j2me_gfx.h"
 
 PSP_MODULE_INFO("funtetris", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
 
-#define SCR_W 480
-#define SCR_H 272
-#define STRIDE 512
-
-// VRAM do PSP: 0x04000000 (cached) ou 0x44000000 (uncached)
-// Usamos uncached para GPU ver direto
-#define VRAM ((unsigned int*)0x44000000)
-
 int main(void) {
-    // Diz pro display usar essa VRAM como framebuffer
-    sceDisplaySetMode(0, SCR_W, SCR_H);
-    sceDisplaySetFrameBuf(VRAM, STRIDE, PSP_DISPLAY_PIXEL_FORMAT_8888,
-                          PSP_DISPLAY_SETBUF_IMMEDIATE);
+    j2me_gfx_init();
 
     while (1) {
-        // Escreve direto nos pixels
-        for (int y = 0; y < SCR_H; y++) {
-            unsigned int* row = VRAM + y * STRIDE;
-            for (int x = 0; x < SCR_W; x++) {
-                row[x] = 0xFF201010;  // azul escuro (ABGR)
-            }
-        }
+        j2me_gfx_begin_frame();
+        j2me_gfx_clear(0x101020);
 
-        // Retangulo vermelho GRANDE no centro
-        for (int y = 50; y < 220; y++) {
-            unsigned int* row = VRAM + y * STRIDE;
-            for (int x = 100; x < 380; x++) {
-                row[x] = 0xFF0000FF;  // vermelho (ABGR)
-            }
-        }
+        j2me_gfx_set_color(0xFF0000);
+        j2me_gfx_fill_rect(10, 10, 100, 100);
 
-        // Garante que a GPU veja o que escrevemos
-        sceKernelDcacheWritebackAll();
+        j2me_gfx_set_color(0x00FF00);
+        j2me_gfx_fill_rect(370, 10, 100, 100);
 
-        sceDisplayWaitVblankStart();
+        j2me_gfx_set_color(0x0000FF);
+        j2me_gfx_fill_rect(10, 162, 100, 100);
+
+        j2me_gfx_set_color(0xFFFF00);
+        j2me_gfx_fill_rect(370, 162, 100, 100);
+
+        j2me_gfx_flip();
 
         SceCtrlData pad;
         sceCtrlReadBufferPositive(&pad, 1);
         if (pad.Buttons & PSP_CTRL_START) break;
     }
 
+    j2me_gfx_shutdown();
     sceKernelExitGame();
     return 0;
 }
