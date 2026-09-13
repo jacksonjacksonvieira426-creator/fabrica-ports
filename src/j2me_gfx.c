08c4d3c0be8c15660005f0f8475ad5574cd5047b
jacksonjacksonvieira426-creator <jacksonjacksonvieira426-creator@users.noreply.github.com>
2026-09-13 14:14:06 -0300

    Fase 4: corrige buffers (offsets VRAM) e vertices (packed + sceGuGetMemory)

--- src/j2me_gfx.c
+++ src/j2me_gfx.c
@@ -9,19 +9,14 @@
 #define SCR_WIDTH  J2ME_SCREEN_W
 #define SCR_HEIGHT J2ME_SCREEN_H
 
-static unsigned int __attribute__((aligned(16))) list[65536];
-static void* framebuf[2];
-static int cur_buf = 0;
+static unsigned int __attribute__((aligned(16))) list[262144];
 static unsigned int cur_color_rgb = 0x000000;
 
-typedef struct {
+typedef struct __attribute__((packed)) {
     unsigned short x, y, z;
     unsigned int   color;
 } Vertex;
 
-static Vertex __attribute__((aligned(4))) rect_v[2];
-
-// Converte RGB (0xRRGGBB) do J2ME para o formato ABGR do sceGu
 static unsigned int rgb_to_psp(unsigned int rgb) {
     unsigned int r = (rgb >> 16) & 0xFF;
     unsigned int g = (rgb >>  8) & 0xFF;
@@ -30,15 +25,13 @@ static unsigned int rgb_to_psp(unsigned int rgb) {
 }
 
 void j2me_gfx_init(void) {
-    framebuf[0] = (void*)0x04000000;
-    framebuf[1] = (void*)(0x04000000 + BUF_WIDTH * SCR_HEIGHT * 4);
-
     sceGuInit();
     sceGuStart(GU_DIRECT, list);
 
-    sceGuDrawBuffer(GU_PSM_8888, framebuf[0], BUF_WIDTH);
-    sceGuDispBuffer(SCR_WIDTH, SCR_HEIGHT, framebuf[1], BUF_WIDTH);
-    sceGuDepthBuffer((void*)0x11000000, BUF_WIDTH);
+    // === IMPORTANTE: offsets, nao enderecos absolutos ===
+    sceGuDrawBuffer(GU_PSM_8888, (void*)0, BUF_WIDTH);
+    sceGuDispBuffer(SCR_WIDTH, SCR_HEIGHT, (void*)0x88000, BUF_WIDTH);
+    sceGuDepthBuffer((void*)0x110000, BUF_WIDTH);
 
     sceGuOffset(2048 - (SCR_WIDTH/2), 2048 - (SCR_HEIGHT/2));
     sceGuViewport(2048, 2048, SCR_WIDTH, SCR_HEIGHT);
@@ -50,13 +43,27 @@ void j2me_gfx_init(void) {
     sceGuDisable(GU_LIGHTING);
     sceGuDisable(GU_BLEND);
     sceGuDisable(GU_TEXTURE_2D);
+    sceGuDisable(GU_ALPHA_TEST);
 
     sceGuFinish();
     sceGuSync(0, 0);
+
+    // Limpa os 2 buffers antes de mostrar
+    sceGuStart(GU_DIRECT, list);
+    sceGuClearColor(0xFF000000);
+    sceGuClear(GU_COLOR_BUFFER_BIT);
+    sceGuFinish();
+    sceGuSync(0, 0);
+    sceGuSwapBuffers();
+
+    sceGuStart(GU_DIRECT, list);
+    sceGuClearColor(0xFF000000);
+    sceGuClear(GU_COLOR_BUFFER_BIT);
+    sceGuFinish();
+    sceGuSync(0, 0);
+
     sceDisplayWaitVblankStart();
     sceGuDisplay(GU_TRUE);
-
-    cur_buf = 0;
 }
 
 void j2me_gfx_shutdown(void) {
@@ -76,8 +83,7 @@ void j2me_gfx_clear(unsigned int rgb) {
 void j2me_gfx_flip(void) {
     sceGuFinish();
     sceGuSync(0, 0);
-    framebuf[cur_buf] = sceGuSwapBuffers();
-    cur_buf ^= 1;
+    sceGuSwapBuffers();
     sceDisplayWaitVblankStart();
 }
 
@@ -90,17 +96,21 @@ void j2me_gfx_fill_rect(int x, int y, int w, int h) {
 
     unsigned int c = rgb_to_psp(cur_color_rgb);
 
-    rect_v[0].x = (unsigned short)x;
-    rect_v[0].y = (unsigned short)y;
-    rect_v[0].z = 0;
-    rect_v[0].color = c;
+    // Pega memoria da lista de comandos atual
+    Vertex* v = (Vertex*)sceGuGetMemory(2 * sizeof(Vertex));
+    if (!v) return;
 
-    rect_v[1].x = (unsigned short)(x + w);
-    rect_v[1].y = (unsigned short)(y + h);
-    rect_v[1].z = 0;
-    rect_v[1].color = c;
+    v[0].x = (unsigned short)x;
+    v[0].y = (unsigned short)y;
+    v[0].z = 0;
+    v[0].color = c;
+
+    v[1].x = (unsigned short)(x + w);
+    v[1].y = (unsigned short)(y + h);
+    v[1].z = 0;
+    v[1].color = c;
 
     sceGuDrawArray(GU_SPRITES,
                    GU_VERTEX_16BIT | GU_COLOR_8888 | GU_TRANSFORM_2D,
-                   2, 0, rect_v);
+                   2, 0, v);
 }
